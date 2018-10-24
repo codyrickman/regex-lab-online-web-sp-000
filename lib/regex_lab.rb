3867bcb3d64cbac35571ca17bef67115a3a3f398
Cody Rickman <cody@creativemindstudios.com>
2018-10-24 00:27:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,17 +7,33 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if (text.scan(un+\w+ing))
+    return true
+  else
+    return false
+  end
 end
 
 def words_five_letters_long(text)
-
+  if (text.scan(\w{5,}))
+    return true
+  else
+    return false
+  end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if (text.scan(\A[A-Z]*\b[[:punct:]]))
+    return true
+  else
+    return false
+  end
 end
 
-def valid_phone_number?(phone)
-
+def valid_phone_number?(text)
+  if (text.scan([0-9]{3}-[0-9]{3}-[0-9]{4}))
+    return true
+  else
+    return false
+  end
 end
