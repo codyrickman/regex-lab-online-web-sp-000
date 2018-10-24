19b32be6e89e83662a1b5c17761258fd6f9a76c3
Cody Rickman <cody@creativemindstudios.com>
2018-10-24 01:13:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,23 +7,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  word_match = []
-  text.each do |word|
-    if (word.scan(/un+\w+ing/) == true)
-      word_match << word
-    end
-  end
-  puts word_match
+  word.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-  word_match = []
-  text.each do |word|
-    if (word.scan(/\w{5,}/) == true)
-      word_match << word
-    end
-  end
-  puts word_match
+  word.scan(/\w{5,}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
